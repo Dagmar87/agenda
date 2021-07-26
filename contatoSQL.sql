61cbf84297d50f634558fba990a9aa2be7a0ff48
José Dagmar Florentino da Silva Sobrinho <64164006+Dagmar87@users.noreply.github.com>
2021-07-26 10:26:59 -0300

    Update contatoSQL.sql

--- contatoSQL.sql
+++ contatoSQL.sql
@@ -3,7 +3,7 @@ CREATE DATABASE agenda;
 USE agenda;
 
 CREATE TABLE contato(
-	id int auto_increment not null primary key,
+    id int auto_increment not null primary key,
     prinome varchar(255) not null,
     ulnome varchar(255) not null,
     email varchar(255) not null,
